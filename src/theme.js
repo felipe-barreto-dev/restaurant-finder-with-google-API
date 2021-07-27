f42b3e2b3d100a85de2d8379c1410952cc88e09c
felipe-barreto-dev <fbarreto.dev@gmail.com>
2021-07-27 13:24:57 -0300

    add home page

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+        primary: '#6200cc',
+        background: '#00000016',
+        text: '#000000',
+        success: '135ed1' 
+    },
+    fonts: {
+        regular: 'Roboto, sans serif',
+    },
+};
+
+export default theme;
